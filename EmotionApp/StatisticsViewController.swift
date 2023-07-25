a09e8cd7c26965c8c2f4f8edea947c1489df7e8b
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-07-25 23:02:52 +0900

    Docs: ViewController 파일 추가 및 각 UIViewController와 연결

---
+++ EmotionApp/StatisticsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StatisticsViewController.swift
+//  EmotionApp
+//
+//  Created by Andy Lee on 2023/07/25.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ EmotionApp/WriteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WriteViewController.swift
+//  EmotionApp
+//
+//  Created by Andy Lee on 2023/07/25.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
