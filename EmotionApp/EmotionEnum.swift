01f8f608197bd1410ba1d8dba2be4216d0a07b5b
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-07-25 23:53:25 +0900

    Feat: 감정 버튼 클릭 시 해당 버튼의 종류와 클릭 횟수를 출력하는 기능 구현

---
+++ EmotionApp/EmotionEnum.swift
@@ -0,0 +1,12 @@
+//
+//  EmotionEnum.swift
+//  EmotionApp
+//
+//  Created by Andy Lee on 2023/07/25.
+//
+
+import Foundation
+
+enum Emotion: Int, CaseIterable {
+    case cheerful, happy, calm, unhappy, exhausted
+}
--- EmotionApp/WriteViewController.swift
+++ EmotionApp/WriteViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class WriteViewController: UIViewController {
-
+    
+    private var emojiCountList = [Int](
+        repeating: 0,
+        count: Emotion.allCases.count
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    @IBAction func emotionButtonTapped(_ sender: UIButton) {
+        emojiCountList[sender.tag] += 1
+        print("\(Emotion.allCases[sender.tag]): \(emojiCountList[sender.tag])")
+    }
+    
 }
